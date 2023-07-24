f67553d20ea6583cc505d3b4f72cf06331696c67
KS7262 <kolya1999starodub@gmail.com>
2023-07-24 12:27:27 +0300

    Добавьте файлы проекта.

---
+++ Models/Post.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Blog.Models
+{
+    public class Post
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+        
+        [ForeignKey("UserId")]
+        public User User { get; set; }
+        
+        [Required(ErrorMessage ="Введіть текст")]
+        public string Text { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blog.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [Required(ErrorMessage ="Введіть ім'я")]
+        [MinLength(2, ErrorMessage ="Мінімальна довжина 2 символи!")]
+        public string FirstName { get; set; }
+
+        [MinLength(2, ErrorMessage = "Мінімальна довжина 2 символи!")]
+        [Required(ErrorMessage = "Введіть прізвище")]
+        public string LastName { get; set; }
+        
+
+        [Required(ErrorMessage = "Введіть пошту")]
+        [RegularExpression(@"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$", ErrorMessage = "Введіть правильну пошту")]
+        public string Email { get; set; }
+        
+        [Required(ErrorMessage = "Введіть пароль")]
+        [MinLength(8, ErrorMessage = "Мінімальна довжина паролю 8 символів")]
+        [RegularExpression(@"^\S+$", ErrorMessage = "Введіть коректний пароль")]
+        public string Password { get; set; }
+
+    }
+}
